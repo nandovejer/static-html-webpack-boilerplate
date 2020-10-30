2199ff08a937bbb40c8d883864ecf8fef925974d
Fernando Muñoz <1270068+Silencespainter@users.noreply.github.com>
2020-10-30 19:59:32 +0100

    adptation step by step

--- src/js/app.js
+++ src/js/app.js
@@ -4,19 +4,19 @@
 // # JS
 
 // ## JS - Global Modules and Webpai Modules
-// import navMenu from "./modules/navMenu";
-// import card from "./modules/card";
-// import sharetime from "./modules/sharetime";
-// import webapi from "./wepapi/webapi";
+import navMenu from "./modules/navMenu";
+import card from "./modules/card";
+import sharetime from "./modules/sharetime";
+import webapi from "./wepapi/webapi";
 import prepareHTML from "./modules/prepareHTML";
 
 // Init
 document.addEventListener("DOMContentLoaded", () => {
   prepareHTML.init(() => {
-    // document.getElementById("nojs").remove();
-    // navMenu.init();
-    // card.init();
-    // webapi.init();
-    // sharetime.init();
+    document.getElementById("nojs").remove();
+    navMenu.init();
+    //card.init();
+    //webapi.init();
+    //sharetime.init();
   });
 });
